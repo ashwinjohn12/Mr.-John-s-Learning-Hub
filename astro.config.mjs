5298eae954ba9e61f35691ffb280ba43977f8dc9
ashwinjohn12 <ashwinjohn12@gmail.com>
2026-09-05 12:20:15 -0600

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ashwinjohn12.github.io',
-  base: '/Mr.-John-s-Learning-Hub',
+  base: '/Mr.-John-s-Learning-Hub/',
   output: 'static'
 });
